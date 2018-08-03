62b5b42b749a059cdcafc9192e85207bb82d8510
sschoener <sebastian.schoener@chasing-carrots.com>
2018-08-03 16:55:06 +0200

    Fix: BasicTypeReader now ignores case

--- Utilities/TypeReaders/BasicTypeReader.cs
+++ Utilities/TypeReaders/BasicTypeReader.cs
@@ -7,7 +7,7 @@ namespace DiscordTestBot
     public class BasicTypeReader : TypeReader {
         public override Task<TypeReaderResult> Read(ICommandContext context, string input, IServiceProvider services)
         {
-            if (Enum.TryParse<BasicType>(input, out BasicType type))
+            if (Enum.TryParse<BasicType>(input, true, out BasicType type))
                 return Task.FromResult(TypeReaderResult.FromSuccess(type));
             return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, $"`{input}` is not a valid basic type!"));
         }
